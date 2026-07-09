b9b3b2c24d225966387918ce5a3d9f71846a38e2
silalahi klery johansen <kleryjohansen09@gmail.com>
2026-07-09 17:50:09 +0700

    Initial Commit

---
+++ watchpairworkout/watchpairworkoutApp.swift
@@ -0,0 +1,17 @@
+//
+//  watchpairworkoutApp.swift
+//  watchpairworkout
+//
+//  Created by silalahi klery johansen on 09/07/26.
+//
+
+import SwiftUI
+
+@main
+struct watchpairworkoutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
